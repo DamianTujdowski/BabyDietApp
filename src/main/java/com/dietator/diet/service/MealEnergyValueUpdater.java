536f990df24ee6bddd5a53843f4be33d06d46bb8
DamianTujdowski <d.tujdowski@gmail.com>
2020-11-25 23:38:05 +0100

    Added tests for updating meal energy when deleting ingredients or updating their values.

--- src/main/java/com/dietator/diet/service/MealEnergyValueUpdater.java
+++ src/main/java/com/dietator/diet/service/MealEnergyValueUpdater.java
@@ -3,53 +3,19 @@ package com.dietator.diet.service;
 import com.dietator.diet.domain.Ingredient;
 import com.dietator.diet.domain.Meal;
 import com.dietator.diet.error.EntityNotFoundException;
-import com.dietator.diet.projections.IngredientBasicInfo;
 import com.dietator.diet.repository.IngredientRepository;
 import com.dietator.diet.repository.MealRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
-public class IngredientService {
-
-    private final IngredientRepository ingredientRepository;
+public class MealEnergyValueUpdater {
 
     private final MealRepository mealRepository;
-
-    public Ingredient findIngredientById(long id) {
-        return ingredientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Ingredient.class, id));
-    }
-
-    public List<IngredientBasicInfo> findAll() {
-        return ingredientRepository.findAllBy();
-    }
-
-    public Ingredient save(Ingredient ingredient) {
-        return ingredientRepository.save(ingredient);
-    }
-
-    @Transactional
-    public Ingredient edit(Ingredient ingredient) {
-        Ingredient editedIngredient = ingredientRepository.findById(ingredient.getId())
-                .orElseThrow(() -> new EntityNotFoundException(Ingredient.class, ingredient.getId()));
-        editedIngredient.setDesignation(ingredient.getDesignation());
-        editedIngredient.setEnergyPer100Grams(ingredient.getEnergyPer100Grams());
-        editedIngredient.setFavourite(ingredient.isFavourite());
-        editedIngredient.setDisliked(ingredient.isDisliked());
-        updateMealEnergyValue(editedIngredient);
-        return editedIngredient;
-    }
-
-    @Transactional
-    public void delete(long id) {
-        updateMealEnergyValue(id);
-        ingredientRepository.deleteById(id);
-    }
+    private final IngredientRepository ingredientRepository;
 
     private void updateMealEnergyValue(Ingredient editedIngredient) {
         Meal meal = findById(editedIngredient.getMealId());
@@ -87,5 +53,9 @@ public class IngredientService {
 
     private int countEnergyPerMeal(Ingredient ingredient) {
         return (int) Math.round(ingredient.getWeightPerMeal() / 100.0 * ingredient.getEnergyPer100Grams());
+    }
+
+    public Ingredient findIngredientById(long id) {
+        return ingredientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Ingredient.class, id));
     }
 }
--- src/test/java/com/dietator/diet/service/UpdatingMealEnergyValueFieldTest.java
+++ src/test/java/com/dietator/diet/service/UpdatingMealEnergyValueFieldTest.java
@@ -34,15 +34,15 @@ public class UpdatingMealEnergyValueFieldTest {
     @Mock
     private PredefinedIngredientCopyingService predefinedIngredientCopyingServiceMock;
 
-    private Meal noIngredientNoConTimesMealFromDb, potatoCucumberBeforeAfternoonMealFromClient,
-            twoIngredientTwoConTimesMealFromDb, fourIngredientFourConTimesMealFromClient,
-            fourIngredientFourConTimesMealFromDb, carrotSugarAfterAfternoonMealFromClient,
-            fourIngredientMealWithTwoPredefinedIngredientsFromClient;
+    private Meal twoIngredientTwoConTimesMealFromDb, fourIngredientFourConTimesMealFromDb,
+            carrotSugarAfterAfternoonMealFromClient;
 
-    private Ingredient potato;
+    private Ingredient potato, potatoFromClient, potatoFromDb;
 
     @BeforeEach
     private void init() {
+        potatoFromDb = new Ingredient(1L, "potato", 340, 180, false, false, false, 5L);
+        potatoFromClient = new Ingredient(1L, "potato", 500, 180, false, false, false, 5L);
         potato = new Ingredient(1L, "potato", 340, 180, false, false, false, 5L);
         Ingredient cucumber = new Ingredient(2L, "cucumber", 150, 100, true, false, false, 5L);
         Ingredient carrot = new Ingredient(3L, "carrot", 98, 70, true, false, false, 5L);
@@ -50,17 +50,11 @@ public class UpdatingMealEnergyValueFieldTest {
         Set<Ingredient> potatoCucumber = Stream.of(potato, cucumber).collect(Collectors.toSet());
         Set<Ingredient> carrotSugar = Stream.of(carrot, sugar).collect(Collectors.toSet());
         Set<Ingredient> fourIngredients = Stream.of(potato, cucumber, carrot, sugar).collect(Collectors.toSet());
-        noIngredientNoConTimesMealFromDb = new Meal(5L, "burrito", 0, "roll cake",
-                10, new HashSet<>(), new HashSet<>(), MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
-        potatoCucumberBeforeAfternoonMealFromClient = new Meal(5L, "burrito", 800, "roll cake",
-                10, new HashSet<>(), potatoCucumber, MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
         carrotSugarAfterAfternoonMealFromClient = new Meal(5L, "burrito", 113, "roll cake",
                 10, new HashSet<>(), carrotSugar, MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
         twoIngredientTwoConTimesMealFromDb = new Meal(5L, "burrito", 762, "roll cake",
                 10, new HashSet<>(), potatoCucumber, MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
         fourIngredientFourConTimesMealFromDb = new Meal(5L, "burrito", 0, "roll cake",
-                10, new HashSet<>(), fourIngredients, MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
-        fourIngredientFourConTimesMealFromClient = new Meal(5L, "burrito", 800, "roll cake",
                 10, new HashSet<>(), fourIngredients, MealCategory.DINNER, PreparationDifficulty.EASY, false, 1);
     }
 
@@ -100,12 +94,8 @@ public class UpdatingMealEnergyValueFieldTest {
         when(mealRepositoryMock.findById(5L)).thenReturn(Optional.of(twoIngredientTwoConTimesMealFromDb));
         when(ingredientRepositoryMock.findById(1L)).thenReturn(Optional.of(potato));
         Meal editedMeal = twoIngredientTwoConTimesMealFromDb;
-        System.out.println(editedMeal.getEnergy());
-        System.out.println(editedMeal.getIngredients().size());
         //when
         ingredientService.delete(1);
-        System.out.println(editedMeal.getEnergy());
-        System.out.println(editedMeal.getIngredients().size());
         //then
         assertEquals(150, editedMeal.getEnergy());
     }
@@ -113,31 +103,13 @@ public class UpdatingMealEnergyValueFieldTest {
     @Test
     public void whenUpdatingIngredientEnergyPer100Grams_shouldUpdateMealEnergyValue() {
         //given
+        when(ingredientRepositoryMock.findById(1L)).thenReturn(Optional.of(potatoFromDb));
         when(mealRepositoryMock.findById(5L)).thenReturn(Optional.of(twoIngredientTwoConTimesMealFromDb));
-        when(ingredientRepositoryMock.findById(1L)).thenReturn(Optional.of(potato));
         Meal meal = twoIngredientTwoConTimesMealFromDb;
-        System.out.println(meal.getEnergy());
-        System.out.println(meal.getIngredients().size());
         //when
-        potato.setEnergyPer100Grams(500);
-        ingredientService.edit(potato);
-        System.out.println(meal.getEnergy());
-        System.out.println(meal.getIngredients().size());
+        ingredientService.edit(potatoFromClient);
         //then
         assertEquals(1050, meal.getEnergy());
-    }
-
-    @Test
-    public void print() {
-        Ingredient cucumber = new Ingredient(2L, "cucumber", 150, 100, true, false, false, 5L);
-        Ingredient carrot = new Ingredient(3L, "carrot", 98, 70, true, false, false, 5L);
-        Ingredient sugar = new Ingredient(4L, "sugar", 315, 14, false, false, false, 5L);
-        Set<Ingredient> ingredients = Stream.of(cucumber, carrot, sugar).collect(Collectors.toSet());
-        System.out.println(ingredients.size());
-        sugar.setEnergyPer100Grams(1000);
-        ingredients.remove(sugar);
-        System.out.println(ingredients.size());
-
     }
 
 }
